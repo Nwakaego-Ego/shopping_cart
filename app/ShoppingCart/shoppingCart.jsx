56a98c0ae8519b0955fa2ebca153fcdfda57cf90
Ego Nwaekpe <glorynwaekpe@gmail.com>
2023-11-13 06:28:45 -0800

    shopping cart

--- app/ShoppingCart/shoppingCart.jsx
+++ app/ShoppingCart/shoppingCart.jsx
@@ -6,14 +6,35 @@ const reducer = (state, action) => {
     case "increment":
       return { count: state.count + 1 };
     case "decrement":
-      return { count: state.count + 1 };
+      return { count: state.count - 1 };
+    case "products":
+      return { products: action.payload };
     default:
       console.log(error);
   }
 };
 
 const shoppingCart = () => {
-  const [state, dispatch] = useReducer(reducer, { count: 0 });
+  const [state, dispatch] = useReducer(reducer, {
+    count: 0,
+     products = [
+    {
+      name: "coke",
+      quantity: 8,
+      price: 300,
+    },
+    {
+      name: "clothe",
+      quantity: 10,
+      price: 500,
+    },
+    {
+      name: "food",
+      quantity: 12,
+      price: 800,
+    }
+  ]
+  }
 
   const increment = () => {
     dispatch({ type: "increment" });
@@ -23,12 +44,24 @@ const shoppingCart = () => {
     dispatch({ type: "decrement" });
   };
 
+
+
   return (
     <>
       <div>Hello World</div>
       <div>{state.count}</div>
       <button onClick={increment}>Increment</button>
       <button onClick={decrement}>Decrement</button>
+      <div>
+        {products.map((product) => {
+          return (
+            <div key={product.id}>
+              {product.}
+              <button>Add to cart</button>
+            </div>
+          );
+        })}
+      </div>
     </>
   );
 };

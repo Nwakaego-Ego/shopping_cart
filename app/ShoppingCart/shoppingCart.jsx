2c590a1e8fc658a904d76de27de69a4569df6461
Ego Nwaekpe <glorynwaekpe@gmail.com>
2023-11-13 16:29:20 -0800

    edit button

--- app/ShoppingCart/shoppingCart.jsx
+++ app/ShoppingCart/shoppingCart.jsx
@@ -1,96 +1,27 @@
-// "use client";
-// import { useReducer } from "react";
-
-// const reducer = (state, action, payload) => {
-//   switch (action.type) {
-//     case "increment":
-//       return { ...state, count: state.count + 1 };
-//     case "decrement":
-//       return { ...state, count: state.count - 1 };
-//     case "products":
-//       return { ...state, products: payload.products };
-//     default:
-//       console.log(error);
-//   }
-// };
-
-// const shoppingCart = () => {
-//   const [state, dispatch] = useReducer(reducer, {
-//     count: 0,
-//     products: [
-//       {
-//         name: "coke",
-//         quantity: 8,
-//         price: 300,
-//       },
-//       {
-//         name: "clothe",
-//         quantity: 10,
-//         price: 500,
-//       },
-//       {
-//         name: "food",
-//         quantity: 12,
-//         price: 800,
-//       },
-//     ],
-//   });
-
-//   const increment = () => {
-//     dispatch({ type: "increment" });
-//   };
-
-//   const decrement = () => {
-//     dispatch({ type: "decrement" });
-//   };
-
-//   const productList = () => {
-//     dispatch({ type: "product", payload: state.products });
-//   };
-
-//   return (
-//     <>
-//       <div>Hello World</div>
-//       <div>{state.count}</div>
-//       <button onClick={increment}>Increment</button>
-//       <button onClick={decrement}>Decrement</button>
-//       <div>
-//         {state.products.map((product) => {
-//           return (
-//             <div key={product.id}>
-//               {product.name} {product.quantity} {product.price}
-//               <button onClick={productList}>Add to cart</button>
-//             </div>
-//           );
-//         })}
-//       </div>
-//     </>
-//   );
-// };
-
-// export default shoppingCart;
 "use client";
+import { useReducer } from "react";
+import { FaEdit } from "react-icons/fa";
 
-import React, { useReducer } from "react";
-
-// Reducer function
-const cartReducer = (state, action) => {
+const reducer = (state, action) => {
   switch (action.type) {
-    case "ADD_TO_CART":
-      return {
-        ...state,
-        cartItems: [...state.cartItems, action.payload],
-      };
+    case "increment":
+      return { ...state, count: state.count + 1 };
+    case "decrement":
+      return { ...state, count: state.count - 1 };
+    case "Add_to_cart":
+      return { ...state, cartItem: [...state.cartItem, action.payload] };
+    case "Edit":
+      return { ...state, editItem: [...state.edit, action.payload] };
     default:
-      return state;
+      console.log(error);
   }
 };
 
-const ShoppingCart = () => {
-  // State and dispatch using useReducer
-  const [state, dispatch] = useReducer(cartReducer, {
+const shoppingCart = () => {
+  const [state, dispatch] = useReducer(reducer, {
     count: 0,
-    cartItems: [],
+    cartItem: [],
+    editItem: [],
     products: [
       {
         name: "coke",
@@ -110,48 +41,101 @@ const ShoppingCart = () => {
     ],
   });
 
-  // Function to add an item to the cart
-  const addToCart = (item) => {
-    dispatch({ type: "ADD_TO_CART", payload: item });
+  const increment = () => {
+    dispatch({ type: "increment" });
   };
 
-  // Calculate total price
-  const calculateTotalPrice = () => {
-    return state.cartItems.reduce(
+  const decrement = () => {
+    dispatch({ type: "decrement" });
+  };
+
+  const cartLog = (item) => {
+    dispatch({ type: "Add_to_cart", payload: item });
+  };
+
+  const totalPrice = () => {
+    return state.cartItem.reduce(
       (total, item) => total + item.price * item.quantity,
       0
     );
   };
 
+  function handleUpdate(index, newItem) {
+    const editCartItem = [...state.cartItem];
+    editCartItem[index] = newItem;
+    dispatch({ type: "Edit", payload: editCartItem });
+  }
+
   return (
-    <div>
-      <h2>Shopping Cart</h2>
-      <div>
-        {state.cartItems.map((item, index) => (
+    <>
+      <div>Hello World</div>
+      <div>{state.count}</div>
+      <button
+        onClick={increment}
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Increment
+      </button>
+      <button
+        onClick={decrement}
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Decrement
+      </button>
+
+      {state.cartItem.map((item, index) => {
+        return (
           <div key={index}>
-            <p>
-              {item.name} - Quantity: {item.quantity} - Price: $
-              {item.price * item.quantity}
-            </p>
+            <div>
+              {item.name}
+              <button
+                onClick={() => {
+                  const newItem = prompt("Enter a new value ");
+                  if (newItem !== null) {
+                    handleUpdate(index, newItem);
+                  }
+                }}
+              >
+                <FaEdit className="paste" />
+              </button>
+            </div>
+            <div>
+              {item.quantity}
+              <button
+                onClick={() => {
+                  const newItem = prompt("Enter a new value ");
+                  if (newItem !== null) {
+                    handleUpdate(index, newItem);
+                  }
+                }}
+              >
+                <FaEdit className="paste" />
+              </button>
+            </div>
+            <div> {item.price}</div>
           </div>
-        ))}
-      </div>
-      <p>Total Price: ${calculateTotalPrice()}</p>
-      {/* Product list */}
+        );
+      })}
+
+      <div>Total Price : ${totalPrice()}</div>
+
       <div>
-        <h2>Product List</h2>
-        <ul>
-          {state.products.map((product, index) => (
-            <li key={index}>
-              {product.name} - Quantity: {product.quantity} - Price: $
-              {product.price}
-              <button onClick={() => addToCart(product)}>Add to Cart</button>
-            </li>
-          ))}
-        </ul>
+        {state.products.map((product) => {
+          return (
+            <div key={product.id}>
+              {product.name} {product.quantity} {product.price}
+              <button
+                onClick={() => cartLog(product)}
+                class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              >
+                Add to cart
+              </button>
+            </div>
+          );
+        })}
       </div>
-    </div>
+    </>
   );
 };
 
-export default ShoppingCart;
+export default shoppingCart;

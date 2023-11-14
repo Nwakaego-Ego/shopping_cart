c3a44187d0189a024852dec22e37fa9c27e44a27
Ego Nwaekpe <glorynwaekpe@gmail.com>
2023-11-14 11:23:25 -0800

    all set with the jsx

--- app/ShoppingCart/shoppingCart.jsx
+++ app/ShoppingCart/shoppingCart.jsx
@@ -1,3 +1,4 @@
+"use client";
 import React, { useReducer, useState } from "react";
 import { FaEdit, FaTrash } from "react-icons/fa";
 
@@ -11,6 +12,8 @@ const reducer = (state, action) => {
       return { ...state, cartItem: action.payload };
     case "Delete_Cart":
       return { ...state, cartItem: action.payload };
+    case "Unit_Price":
+      return { ...state, products: action.payload };
     default:
       console.error("Unknown action type");
       return state;
@@ -24,6 +27,7 @@ const ShoppingApp = () => {
     editItem: [],
     deleteItem: [],
     products: [],
+    unit: [],
   });
 
   const [newProduct, setNewProduct] = useState({
@@ -32,22 +36,30 @@ const ShoppingApp = () => {
     price: 0,
   });
 
+  const [price, setPrice] = useState();
+
   const handleChange = (e) => {
-    setNewProduct({ ...newProduct, [e.target.name]: e.target.value });
+    if (e.target.name === "quantity") {
+      const quantity = parseInt(e.target.value, 10);
+      const newPrice = quantity * 10;
+      setPrice(newPrice);
+      setNewProduct({
+        ...newProduct,
+        quantity,
+      });
+    } else {
+      setNewProduct({ ...newProduct, [e.target.name]: e.target.value });
+    }
   };
 
-  const addProduct = () => {
-    dispatch({ type: "Add_Product", payload: newProduct });
-    setNewProduct({ name: "", quantity: 0, price: 0 });
-  };
-
-  const cartLog = (item) => {
-    dispatch({ type: "Add_to_cart", payload: item });
+  const cartLog = () => {
+    dispatch({ type: "Add_to_cart", payload: newProduct });
+    setNewProduct({ name: "", quantity: 0, price });
   };
 
   const totalPrice = () => {
     return state.cartItem.reduce(
-      (total, item) => total + item.price * item.quantity,
+      (total, item) => total + price * item.quantity,
       0
     );
   };
@@ -88,7 +100,7 @@ const ShoppingApp = () => {
               <FaEdit className="paste" />
             </button>
           </div>
-          <div>
+          {/* <div>
             {item.price}
             <button
               onClick={() => {
@@ -100,7 +112,8 @@ const ShoppingApp = () => {
             >
               <FaEdit className="paste" />
             </button>
-          </div>
+          </div> */}
+          <div>{price}</div>
           <div>{item.quantity}</div>
           <div>
             <button onClick={() => handleDelete(index)}>
@@ -111,21 +124,6 @@ const ShoppingApp = () => {
       ))}
 
       <div>Total Price : ${totalPrice()}</div>
-
-      <div>
-        {state.products.map((product, index) => (
-          <div key={index}>
-            {product.name} {product.quantity} {product.price}
-            <button
-              onClick={() => cartLog(product)}
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-            >
-              Add to cart
-            </button>
-          </div>
-        ))}
-      </div>
-
       <div>
         <h2>Add a New Product</h2>
         <label>Name:</label>
@@ -146,10 +144,15 @@ const ShoppingApp = () => {
         <input
           type="number"
           name="price"
-          value={newProduct.price}
+          value={price}
           onChange={handleChange}
         />
-        <button onClick={addProduct}>Add Product</button>
+        <button
+          onClick={cartLog}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Add to cart
+        </button>
       </div>
     </>
   );
